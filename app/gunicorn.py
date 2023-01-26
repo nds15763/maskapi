7c191b079a006de78f0cde3483bfb2e9898431a4
unknown <425257452@qq.com>
2023-01-26 17:07:38 +0800

    Edit:添加守护文件

---
+++ app/gunicorn.py
@@ -0,0 +1,11 @@
+daemon=True #是否守护
+bind='0.0.0.0:8080'#绑定
+pidfile='gunicorn.pid'#pid文件地址
+chdir='.' # 项目地址
+worker_class='uvicorn.workers.UvicornWorker'
+workers=1
+threads=2
+loglevel='debug' # 日志级别
+access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
+accesslog = "gunicorn_access.log"
+errorlog = "gunicorn_error.log"
