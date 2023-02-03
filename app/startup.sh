4107deee55ed10942de10859e65384cf53002bcd
unknown <425257452@qq.com>
2023-02-03 18:24:42 +0800

    Edit:添加启动脚本

---
+++ app/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ps -ef|grep gunicorn|grep -v grep|awk '{print $2}'
+if [ -n "$pid"]
+then
+    echo "kill -9 pid:" $pid
+    kill -9 $pid
+fi
+git pull
+gunicorn main:app --bind 0.0.0.0:8080 --worker-class uvicorn.workers.UvicornWorker
